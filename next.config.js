597a57217f94c6213dd11149107b0ed9e9c91e4f
newton imonjirie <newtonimonjirie@gmail.com>
2021-06-21 08:50:39 +0100

    fixed ts issue

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
   images: {
     domains: ["storage.googleapis.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api-prod.autochek.africa/v1/inventory/:path*", // Proxy to Backend
+      },
+    ];
+  },
 };
