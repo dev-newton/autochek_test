d040f58ab1dd694546d3b5dc4db65358b37d70fd
newton imonjirie <newtonimonjirie@gmail.com>
2021-06-21 12:37:51 +0100

    Fixed TS errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,36 @@
 import Layout from "../components/Layout";
 import { BASE_API_URL } from "../config";
+import axios from 'axios'
 
 import ImageSlider from "../components/ImageSlider";
 import CarList from "../components/CarList/CarList";
 import { GetServerSideProps } from "next";
 
-function Home({ carMakes }) {
+function Home({ carMakes, carList }) {
   // console.log('TEST 1: ',carList )
   return (
     <div>
       <Layout title="Autochek Test">
         <ImageSlider carMakes={carMakes} />
-        {/* <CarList carList={carList} /> */}
+        <CarList carList={carList} />
       </Layout>
     </div>
   );
 }
 
 export const getServerSideProps:GetServerSideProps = async () => {
-  const res = await fetch(`${BASE_API_URL}/car/search`);
+  const res = await axios.get(`${BASE_API_URL}/car/search`);
   const res_1 = await fetch(`${BASE_API_URL}/make?popular=true`);
 
 
-  // const carList = await res.json();
+  const carList = res.data;
   const carMakes = await res_1.json();
 
-  console.log(119, await res.json())
+  console.log(153, res.data)
 
   return {
     props: {
-      // carList,
+      carList,
       carMakes,
     },
   };
